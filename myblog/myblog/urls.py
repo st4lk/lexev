3f1e095518041f98146ac8d310e52869329d512e
st4lk <myhappydo@gmail.com>
2013-02-17 14:27:35 +0400

    remove unused i18n functions

--- myblog/myblog/urls.py
+++ myblog/myblog/urls.py
@@ -14,7 +14,6 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^i18n/', include('django.conf.urls.i18n')),
 )
 
 from haystack.forms import ModelSearchForm
